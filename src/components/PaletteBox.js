f9c74a85d6c1043819a532b9dec8279268e9e22f
Ash Taylor <a@zi.vc>
2022-10-28 10:36:41 +0100

    wip: progress commit

--- src/components/PaletteBox.js
+++ src/components/PaletteBox.js
@@ -1,12 +1,23 @@
 import {Box, Text} from "@chakra-ui/react";
 import chroma from "chroma-js";
+import {useMemo} from "react";
 
 
 const WHITE = chroma("white"),
     BLACK = chroma("black");
 
 const PaletteBox = ({prefix = '', name, color}) => {
-    const textColor = (chroma.contrast(color, WHITE) >= chroma.contrast(color, BLACK) ? WHITE : BLACK).hex()
+
+    const textColor = useMemo(() => {
+        let _textColor = WHITE;
+        try {
+            _textColor = (chroma.contrast(color, WHITE) >= chroma.contrast(color, BLACK) ? WHITE : BLACK);
+        } catch (e) {
+            // nothing
+        }
+        return _textColor.hex();
+    }, [color]);
+
     return (
         <Box p={4} bgColor={color} width={"100%"}>
             <Text color={textColor}>
